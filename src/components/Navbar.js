2089feb77b05227cbff6bb6562a755fa1e4ddbda
AshourDono <75224277+AshourDono@users.noreply.github.com>
2022-05-08 00:36:56 +0200

    logout functionality, some design tweaks ;D

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,76 @@
-import React from "react";
-import { Link } from "react-router-dom"
-import style from "./style.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { GrLogout } from 'react-icons/gr';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    Cookies.remove('jwt');
+    navigate('/');
+  };
   return (
-    <nav className="navbar navbar-expand navbar-light shadow ">
-      <div className="container-fluid">
-        <div className="navbar-brand fs-4 mx-3" href="#">
-          Welcome
-        </div>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link to="/profile"className="nav-link fs-5 mx-3 rounded-3">
+    <nav className='navbar navbar-expand navbar-light shadow '>
+      <div className='container-fluid flex-row'>
+        <div className='collapse navbar-collapse col-2' id='navbarNavAltMarkup'>
+          <div className='navbar-brand fs-4 ms-2 me-3' href='#'>
+            Welcome
+          </div>
+          <div className='navbar-nav w-100'>
+            <Link to='/profile' className='nav-link fs-5  mx-2 rounded-3'>
               User Profile
             </Link>
-            <Link to="/projects" className="nav-link fs-5 mx-3 rounded-3">
+            <Link to='/projects' className='nav-link fs-5  mx-2 rounded-3'>
               Projects
             </Link>
-            
-            <div className="dropdown ">
-                    <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"/>
-                        <strong>mdo</strong>
-                    </a>
 
-                    <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><a className="dropdown-item" href="#">New project...</a></li>
-                        <li><a className="dropdown-item" href="#">Settings</a></li>
-                        <li><a className="dropdown-item" href="#">Profile</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
+            <div className='dropdown my-auto' style={{ marginLeft: '70%' }}>
+              <Link
+                to='#'
+                className='d-flex align-items-center text-white text-decoration-none dropdown-toggle'
+                id='dropdownUser1'
+                data-bs-toggle='dropdown'
+                aria-expanded='false'>
+                <img
+                  src='https://github.com/mdo.png'
+                  alt=''
+                  width='40'
+                  height='40'
+                  className='rounded-circle me-2'
+                />
+                <strong>mdo</strong>
+              </Link>
+
+              <ul
+                className='dropdown-menu dropdown-menu-white text-small shadow me-0'
+                aria-labelledby='dropdownUser1'>
+                <li>
+                  <Link className='dropdown-item' to='#'>
+                    New project...
+                  </Link>
+                </li>
+                <li>
+                  <Link className='dropdown-item' to='#'>
+                    Settings
+                  </Link>
+                </li>
+                <li>
+                  <Link className='dropdown-item' to='#'>
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <hr className='dropdown-divider' />
+                </li>
+                <li onClick={handleLogout}>
+                  <div className='dropdown-item' role='button'>
+                    <GrLogout className='text-white me-1' />
+                    Sign out
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/userComponents/InfoComponent.js
+++ src/userComponents/InfoComponent.js
@@ -1,126 +1,145 @@
-import React ,{ useState, useEffect }from "react";
-import axiosInstance from '../network/axiosConfig';
-import dodo from "../images/1.jpg"
-import {AiTwotoneMail} from "react-icons/ai"
-import{AiFillFlag} from "react-icons/ai"
-import{BsFillCalendarDateFill} from"react-icons/bs"
+// import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
+import { AiFillFlag } from 'react-icons/ai';
+import { BsFillCalendarDateFill } from 'react-icons/bs';
+import { AiTwotoneMail } from 'react-icons/ai';
 
-export default function InfoComponent({userData,userImg}) {
+import axiosInstance from '../network/axiosConfig';
+
+export default function InfoComponent({ userData, userImg }) {
   const navigate = useNavigate();
 
-  const userDelete = (e) => {
+  const userDelete = e => {
     e.preventDefault();
-    axiosInstance
-      .delete('users/delete',{ withCredentials: true })
-      .then(res => {
-        Cookies.remove("jwt")
-        navigate('/');
-
-    
-      });
+    axiosInstance.delete('users/delete', { withCredentials: true }).then(res => {
+      Cookies.remove('jwt');
+      navigate('/');
+    });
   };
-  
+
   return (
-    <>
-     
-
-
-      <div className="col-md-12 offset-md-1 mt-5 shadow">
-        <div className="card card-view">
-          <div className="card-header text-white " style={{ backgroundColor: "#354f6f" }}>
-            <h1 className="text-center py-4 fw-bold"> <i className=" fa-solid fa-person-running nav-icon"></i>  {`${userData.first_name} ${userData.last_name}`}  <i className="fa-solid fa-person-running nav-icon"></i> </h1>
-
+    <div className='col-md-12 offset-md-1 mt-5 shadow'>
+      <div className='card card-view'>
+        <div className='card-header text-white ' style={{ backgroundColor: '#354f6f' }}>
+          <h1 className='text-center py-4 fw-bold'>
+            {' '}
+            <i className=' fa-solid fa-person-running nav-icon'></i>{' '}
+            {`${userData.first_name} ${userData.last_name}`}{' '}
+            <i className='fa-solid fa-person-running nav-icon'></i>{' '}
+          </h1>
+        </div>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img
+              src={userImg}
+              className=' m-5'
+              alt=''
+              style={{
+                width: '100px',
+                height: '100px',
+                borderRadius: '50%',
+                border: '3px solid #354F6F',
+              }}
+            />
           </div>
-          <div className="row">
-
-            <div className="col-md-4">
-              <img src={userImg} className=" m-5" style={{ width: "100px", height: "100px", borderRadius: "50%", border: "3px solid #354F6F" }} />
-
-            </div>
-            <div className="col-md-8 d-flex align-items-center">
-              <div className="card-body ">
-
-                <div className="row" >
-                <div className="col-sm-4 border-right ">
-                    <div className="description-block">
-                      <h5 className="description-header"><i className="fa-solid fa-mobile-screen"></i> Mobile:</h5>
-                      <p className="description-text mt-3">{userData.mobile_phone}</p>
-                    </div>
-
+          <div className='col-md-8 d-flex align-items-center'>
+            <div className='card-body '>
+              <div className='row'>
+                <div className='col-sm-4 border-right '>
+                  <div className='description-block'>
+                    <h5 className='description-header'>
+                      <i className='fa-solid fa-mobile-screen'></i> Mobile:
+                    </h5>
+                    <p className='description-text mt-3'>{userData.mobile_phone}</p>
                   </div>
-               
-                 
-                  <div className="col-sm-4 ">
-                    <div className="description-block">
-                      <h5 className="description-header"> <AiFillFlag className="mx-1"/>Country </h5>
-                      <p className="description-text mt-3">{userData.country}</p>
-                    </div>
+                </div>
 
+                <div className='col-sm-4 '>
+                  <div className='description-block'>
+                    <h5 className='description-header'>
+                      {' '}
+                      <AiFillFlag className='mx-1' />
+                      Country{' '}
+                    </h5>
+                    <p className='description-text mt-3'>{userData.country}</p>
                   </div>
-                  <div className="col-sm-4 ">
-                    <div className="description-block">
-                      <h5 className="description-header me-5"><BsFillCalendarDateFill className="me-2"/>Birth Date </h5>
-                      <p className="description-text mt-3">{userData.birthday}</p>
-                    </div>
-
+                </div>
+                <div className='col-sm-4 '>
+                  <div className='description-block'>
+                    <h5 className='description-header me-5'>
+                      <BsFillCalendarDateFill className='me-2' />
+                      Birth Date{' '}
+                    </h5>
+                    <p className='description-text mt-3'>{userData.birthday}</p>
                   </div>
-                  <div className="col-sm-3 mt-3">
-                    <button className="btn btn-danger"data-bs-toggle="modal" data-bs-target="#staticBackdrop" >Delete Profile</button>
-
-                  </div>
-                  <div className="col-sm-3 mt-3 ">
-                    <button className="btn btn-success">Edit Profile</button>
-
-                  </div>
-
+                </div>
+                <div className='col-sm-3 mt-3'>
+                  <button
+                    className='btn btn-danger'
+                    data-bs-toggle='modal'
+                    data-bs-target='#staticBackdrop'>
+                    Delete Profile
+                  </button>
+                </div>
+                <div className='col-sm-3 mt-3 '>
+                  <button className='btn btn-success'>Edit Profile</button>
                 </div>
               </div>
-
             </div>
-             <div className="card-footer text-muted row">
-          <div className="col-sm-5 offset-2">
-                    <div className="description-block border-end">
-                      <p className="description-header me-5"><i className="fa-brands fa-facebook btn-lg"></i> Facebook Profile:</p>
-                    </div>
-
-                  </div>
-                  <div className="col-sm-5 ">
-                    <div className="description-block">
-                      <p className="description-header me-5"><AiTwotoneMail className="mx-3"/> Email: {userData.email} </p>
-                    </div>
-
-                  </div>
-
           </div>
-            
+          <div className='card-footer text-muted row'>
+            <div className='col-sm-5 offset-2'>
+              <div className='description-block border-end'>
+                <p className='description-header me-5'>
+                  <i className='fa-brands fa-facebook btn-lg'></i> Facebook Profile:
+                </p>
+              </div>
+            </div>
+            <div className='col-sm-5 '>
+              <div className='description-block'>
+                <p className='description-header me-5'>
+                  <AiTwotoneMail className='mx-3' /> Email: {userData.email}{' '}
+                </p>
+              </div>
+            </div>
           </div>
-         
         </div>
-
-
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Delete Account</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-      Are you sure you want to delete?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-outline-danger" onClick={(e)=>userDelete(e)}>Delete</button>
+
+      <div
+        className='modal fade'
+        id='staticBackdrop'
+        data-bs-backdrop='static'
+        data-bs-keyboard='false'
+        tabIndex='-1'
+        aria-labelledby='staticBackdropLabel'
+        aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title' id='staticBackdropLabel'>
+                Delete Account
+              </h5>
+              <button
+                type='button'
+                className='btn-close'
+                data-bs-dismiss='modal'
+                aria-label='Close'></button>
+            </div>
+            <div className='modal-body'>Are you sure you want to delete?</div>
+            <div className='modal-footer'>
+              <button type='button' className='btn btn-outline-primary' data-bs-dismiss='modal'>
+                Cancel
+              </button>
+              <button type='button' className='btn btn-outline-danger' onClick={e => userDelete(e)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-      </div>
-
-
-    </>
   );
 }
